17e9917417ad378ad57e729c5e1a7978a04391e5
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-10-11 01:52:18 -0300

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
-import { LeftBarComponent } from './left-bar/left-bar.component';
-import { BreadCrumbComponent } from './breadCrumb/breadCrumb.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'breadCrumb',
-    component: BreadCrumbComponent
-  },
-  {
-    path: 'left-bar-component',
-    component: LeftBarComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

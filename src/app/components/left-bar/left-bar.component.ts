a9daa9a1152e319ab73a76fd4b1dd14f28ac686e
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-10-18 22:29:31 -0300

    update menu e breadcrumb

--- src/app/components/left-bar/left-bar.component.ts
+++ src/app/components/left-bar/left-bar.component.ts
@@ -10,38 +10,6 @@ import { Component, OnInit } from "@angular/core";
 
 export class LeftBarComponent implements OnInit {
 
-    categorias: any = [
-        {ID: 1, nome:'Card Game'},
-        {ID: 2, nome:'Jogos de Tabuleiro'},
-        {ID: 3, nome:'Livros RPG'},
-    ];
-
-    subcategorias: any = [
-        {ID: 1, nome:'Magic', IDPai: 1},
-        {ID: 2, nome:'Pokémon', IDPai: 1},
-        {ID: 3, nome:'Yu-Gi-Oh!', IDPai: 1},
-        {ID: 4, nome:'Cooperativos', IDPai: 2},
-        {ID: 5, nome:'Estratégicos', IDPai: 2},
-        {ID: 6, nome:'Medieval', IDPai: 3},
-        {ID: 7, nome:'Terror', IDPai: 3},
-        {ID: 8, nome:'Futurista', IDPai: 3},
-    ]
-
-    sub2categorias: any =[
-        {ID: 1, nome:'Booster', IDPai: 1},
-        {ID: 2, nome:'Bundle', IDPai: 1},
-        {ID: 3, nome:'Deck Pronto', IDPai: 1},
-        {ID: 4, nome:'Duel Decks', IDPai: 1},
-        {ID: 5, nome:'Blister', IDPai: 2},
-        {ID: 6, nome:'Box Especial', IDPai: 2},
-        {ID: 7, nome:'Boxter', IDPai: 2},
-        {ID: 8, nome:'Kit Treinador', IDPai: 2},
-        {ID: 9, nome:'Latas', IDPai: 2},
-        {ID: 10, nome:'Booster', IDPai: 3},
-        {ID: 11, nome:'Box', IDPai: 3},
-        {ID: 12, nome:'Deck Inicial', IDPai: 3},
-    ]
-
     constructor(private productService: ProductService) {
         /*
             var listCG = new ProductService();
@@ -53,6 +21,38 @@ export class LeftBarComponent implements OnInit {
             return list.filter(item => item.toLowerCase().search(this.pesquisar.toLowerCase())!= -1);
         }*/
     }
+
+  public categorias: any = [
+        {ID: 1, nome:'Card Game'},
+        {ID: 2, nome:'Jogos de Tabuleiro'},
+        {ID: 3, nome:'Livros RPG'},
+    ];
+
+   public subcategorias1: any = [
+        {"nome":'Magic', IDPai: 1},
+        {"nome":'Pokémon', IDPai: 1},
+        {"nome":'Yu-Gi-Oh!', IDPai: 1},
+        {"nome":'Cooperativos', IDPai: 2},
+        {"nome":'Estratégicos', IDPai: 2},
+        {"nome":'Medieval', IDPai: 3},
+        {"nome":'Terror', IDPai: 3},
+        {"nome":'Futurista', IDPai: 3}
+    ]
+
+    public sub2categorias = [
+        {ID: 1, "nome":'Booster', IDPai: 1},
+        {ID: 2, "nome":'Bundle', IDPai: 1},
+        {ID: 3, "nome":'Deck Pronto', IDPai: 1},
+        {ID: 4, "nome":'Duel Decks', IDPai: 1},
+        {ID: 5, "nome":'Blister', IDPai: 2},
+        {ID: 6, "nome":'Box Especial', IDPai: 2},
+        {ID: 7, "nome":'Boxter', IDPai: 2},
+        {ID: 8, "nome":'Kit Treinador', IDPai: 2},
+        {ID: 9, "nome":'Latas', IDPai: 2},
+        {ID: 10, "nome":'Booster', IDPai: 3},
+        {ID: 11, "nome":'Box', IDPai: 3},
+        {ID: 12, "nome":'Deck Inicial', IDPai: 3},
+    ]
 
     ngOnInit() {
 

100d92aeef69b4883974db54708101790bf6ccb1
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-10-23 15:11:36 -0300

    add assets img pokemon cardGames

--- src/app/components/pokemon-card/box-especial/box-especial.component.ts
+++ src/app/components/pokemon-card/box-especial/box-especial.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoxEspecialComponent implements OnInit {
 
+  specialBoxes:any=[
+    {
+      tipe: 'Box Especial',
+      pathImage: 'assets/imgPokemon/box_especial/POKEMON BOX BARALHO - BATALHA DE LIGA CHARIZARD VS PIKACHU.png',
+      name: 'POKEMON BOX BATALHA DE LIGA Charizard VS Pikachu',
+      price: 'R$ 149,90',
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {

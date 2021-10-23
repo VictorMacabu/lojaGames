edff16e97e7d69eff35bad0f5f1c007b2b48cba2
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-10-23 20:52:46 -0300

    update cardGames

--- src/app/components/pokemon-card/box-especial/box-especial.component.ts
+++ src/app/components/pokemon-card/box-especial/box-especial.component.ts
@@ -13,7 +13,37 @@ export class BoxEspecialComponent implements OnInit {
       pathImage: 'assets/imgPokemon/box_especial/POKEMON BOX BARALHO - BATALHA DE LIGA CHARIZARD VS PIKACHU.png',
       name: 'POKEMON BOX BATALHA DE LIGA Charizard VS Pikachu',
       price: 'R$ 149,90',
-    }
+    },
+    {
+      tipe: 'Box Especial',
+      pathImage: 'assets/imgPokemon/box_especial/POKEMON BOX COLOSSAL CAMINHO DO CAMPEAO.png',
+      name: 'POKEMON BOX COLOSSAL CAMINHO DO CAMPEAO',
+      price: 'R$ 39,90',
+    },
+    {
+      tipe: 'Box Especial',
+      pathImage: 'assets/imgPokemon/box_especial/POKEMON BOX LAPRAS CAMINHO DO CAMPEAO.png',
+      name: 'POKEMON BOX LAPRAS CAMINHO DO CAMPEAO',
+      price: 'R$ 39,90',
+    },
+    {
+      tipe: 'Box Especial',
+      pathImage: 'assets/imgPokemon/box_especial/POKEMON BOX URSHIFU V GOLPE DECISIVO ESPADA ESCUDO.png',
+      name: 'POKEMON BOX URSHIFU V GOLPE DECISIVO ESPADA ESCUDO',
+      price: 'R$ 99,90',
+    },
+    {
+      tipe: 'Box Especial',
+      pathImage: 'assets/imgPokemon/box_especial/POKEMON BOX URSHIFU V GOLPE FLUIDO ESPADA ESCUDO.png',
+      name: 'POKEMON BOX URSHIFU V GOLPE FLUIDO ESPADA ESCUDO',
+      price: 'R$ 99,90',
+    },
+    {
+      tipe: 'Box Especial',
+      pathImage: 'assets/imgPokemon/box_especial/POKEMON DECK BARALHO - BATALHA DE LIGA ZACIAN V.png',
+      name: 'POKEMON DECK BARALHO - BATALHA DE LIGA ZACIAN V',
+      price: 'R$ 119,90',
+    },
   ]
   constructor() { }
 

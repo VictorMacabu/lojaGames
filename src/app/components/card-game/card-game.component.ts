3f840892edc087f0c1f1ae1e84b0f50d642fcbf5
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-10-25 01:32:43 -0300

    update cardGame

--- src/app/components/card-game/card-game.component.ts
+++ src/app/components/card-game/card-game.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-game.component.css']
 })
 export class CardGameComponent implements OnInit {
-
+  sections: any = [
+    {
+      tipe: 'Magic',
+      router: '/cardGame/magic',
+      pathImage: 'assets/imgCardGames/magic.jpg'
+    },
+    {
+      tipe: 'Pokémon',
+      router: '/cardGame/pokemon',
+      pathImage: 'assets/imgCardGames/pokemon.jpg'
+    },
+    {
+      tipe: 'Yu-Gi-Oh!',
+      router: '/cardGame/yugioh',
+      pathImage: 'assets/imgCardGames/yugioh.jpg'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {

c4f4721bcecc525812076afaf0d7f2e1453214fc
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-10-27 19:30:16 -0300

    update cardgame pokemon

--- src/app/components/magic-card/magic-card.component.ts
+++ src/app/components/magic-card/magic-card.component.ts
@@ -8,21 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class MagicCardComponent implements OnInit {
 
   lugares: any = [
-    {
-      tipe: 'Magic',
-      router: '/cardGame/magic',
-      pathImage: 'assets/imgCardGames/magic.jpg'
+    { 
+      tipe: 'Box Booster',
+      pathImage: 'assets/imgMagic/booster/MAGIC CAIXA DE BOOSTER COLEÇÃO BÁSICA 2021 CORE SET M21.png',
+      router: '/cardGame/magic/booster',
     },
     {
-      tipe: 'Pokémon',
-      router: '/cardGame/pokemon',
-      pathImage: 'assets/imgCardGames/pokemon.jpg'
+      tipe: 'Bundle',
+      pathImage: 'assets/imgMagic/bundle/MAGIC BUNDLE COLEÇÃO BASICA 2020 CORE SET M20 PACOTE.png',
+      router: '/cardGame/magic/bundle',
     },
     {
-      tipe: 'Yu-Gi-Oh!',
-      router: '/cardGame/yugioh',
-      pathImage: 'assets/imgCardGames/yugioh.jpg'
-    }
+      tipe: 'Deck Pronto',
+      pathImage: 'assets/imgMagic/deck_pronto/MAGIC DECK PRE CONSTRUIDO BATALHÃO ALVORECER MOURNINGTIDE.png',
+      name: 'BATALHÃO ALVORECER MOURNINGTIDE',
+      router: '/cardGame/magic/deck_pronto',
+    },
+    {
+      tipe: 'Duel Decks',
+      pathImage: 'assets/imgMagic/duel_decks/MAGIC ARENA KIT INICIAL COLEÇÃO BÁSICA 2021 M21.png',
+      router: '/cardGame/magic/duel_decks',
+    },
   ]
 
   constructor() { }
--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -8,21 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class PokemonCardComponent implements OnInit {
 
   opcoes: any = [
-    {
-      tipe: 'Magic',
-      router: '/cardGame/magic',
-      pathImage: 'assets/imgCardGames/magic.jpg'
+    { 
+      tipe: 'Latas',
+      pathImage: 'assets/imgPokemon/latas/Lata Pokemon Destinos Ocultos Charizard GX.png',
+      router: '/cardGame/pokemon/latas',
     },
-    {
-      tipe: 'Pokémon',
-      router: '/cardGame/pokemon',
-      pathImage: 'assets/imgCardGames/pokemon.jpg'
+    { 
+      tipe: 'Kit Treinador',
+      pathImage: 'assets/imgPokemon/kit_treinador/POKEMON KIT TREINADOR PIKACHU MASCARADA E SUICUNE.png',
+      router: '/cardGame/pokemon/kit_treinador',
     },
-    {
-      tipe: 'Yu-Gi-Oh!',
-      router: '/cardGame/yugioh',
-      pathImage: 'assets/imgCardGames/yugioh.jpg'
-    }
+    { 
+      tipe: 'Boxter',
+      pathImage: 'assets/imgPokemon/boxter/POKEMON BOX - DUBWOOL V CAMINHO DO CAMPEAO.png',
+      router: '/cardGame/pokemon/boxter',
+    },
+    { 
+      tipe: 'Box Especial',
+      pathImage: 'assets/imgPokemon/box_especial/POKEMON BOX LAPRAS CAMINHO DO CAMPEAO.png',
+      router: '/cardGame/pokemon/box_especial',
+    },
+    { 
+      tipe: 'Blister',
+      pathImage: 'assets/imgPokemon/blister/BLISTER TRIPLO POKEMON - ESTILOS DE BATALHA ESPADA E ESCUDO CHARMANDER.png',
+      router: '/cardGame/pokemon/blister',
+    },
+
   ]
 
   constructor() { }
